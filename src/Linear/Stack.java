63b8ccc0edf7dec446982a5fc3a5e15bb5f3c88e
Sonu Kumar <sonukumar03xt@gmail.com>
2020-10-08 21:28:20 +0530

    stack's sort function created

---
+++ src/Linear/Stack.java
@@ -0,0 +1,56 @@
+package Linear;
+
+public class Stack{
+	private int size;
+	private int []ar;
+	private int top=-1;
+	Stack(int size){
+		this.size = size;
+		this.top=-1;
+		this.ar = new int[size];
+	}
+	public void push(int e){
+		top++;
+		ar[top]=e;
+	}
+	public int pop() {
+		int e = ar[top];
+		top--;
+		return e;
+	}
+	boolean isEmplty(){
+		return this.top==-1;
+	}
+	public int peek() {
+		return ar[top];
+	}
+	int getSize() {
+		return this.size;
+	}
+	void print() {
+		for(int i =0;i<=top;i++) {
+			System.out.print(ar[i]+" ");
+		}
+		System.out.println(" ");
+	}
+	 void sort(Stack st1, Stack st2) {
+		while(!st1.isEmplty()) {
+			int e = st1.pop();
+			if(st2.isEmplty()) {
+				st2.push(e);
+			}else if (e<st2.peek()) {
+				st2.push(e);
+			}else {
+				st1.push(st2.pop());
+				st1.push(e);
+
+			}
+			st1.print();
+			st2.print();
+		}
+		if(st1.isEmplty()) {
+			System.out.println("sorted");
+		}
+		st2.print();
+	}
+}
